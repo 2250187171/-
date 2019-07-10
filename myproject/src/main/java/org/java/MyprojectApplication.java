0c2ca2ff59c4c8f2f5cd63b16705ac8c60944ced
597773029 <597773029@qq.com>
2019-07-10 17:33:00 +0800

    Merge branch 'master' of C:\idea\myproject with conflicts.

--- myproject/src/main/java/org/java/MyprojectApplication.java
+++ myproject/src/main/java/org/java/MyprojectApplication.java
@@ -3,6 +3,9 @@ package org.java;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+import java.util.Calendar;
+
 //开启eureka服务
 @EnableEurekaServer
 @SpringBootApplication
@@ -10,6 +13,9 @@ public class MyprojectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MyprojectApplication.class, args);
-    }
 
+        Calendar calendar = Calendar.getInstance();
+
+        calendar.set(Calendar.HOUR_OF_DAY, 8);
+    }
 }
