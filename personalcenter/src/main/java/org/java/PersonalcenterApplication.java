89997b83d863d1cfa5e47638030b4d0630e79d3a
597773029 <597773029@qq.com>
2019-07-10 17:54:48 +0800

    肖本强的第二次提交

---
+++ personalcenter/src/main/java/org/java/PersonalcenterApplication.java
@@ -0,0 +1,13 @@
+package org.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalcenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalcenterApplication.class, args);
+    }
+
+}
